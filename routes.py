53e70669a570ae7bb966aace1864be55bb9bc2f9
Varun Rao <vrao@gogoair.com>
2017-08-12 12:14:02 -0500

    feautre: Include routing function

---
+++ routes.py
@@ -0,0 +1,20 @@
+from flask import Flask, redirect, request, session
+from oauth import OAuth
+app = Flask(__name__)
+app.secret_key = 'be00ce44e8a48addf582665368a2742f0f037313'
+  
+@app.route('/')
+def start():
+    url_root =  request.url_root
+    oauth = OAuth(url_root=url_root)
+    facebook_url = oauth.facebook_url
+    return redirect(facebook_url)
+
+@app.route('/authorized', methods=['GET'])
+def authorized():
+    print("hi")
+    url_root = request.url_root
+    code = request.args.get('code', '')
+    oauth = OAuth(url_root=url_root)
+    oauth.retrieve_access_token(code=code)
+    

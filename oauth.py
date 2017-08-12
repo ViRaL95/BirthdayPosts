6ba2756b97854d960d15bd116a4b1b3c2683af3c
Varun Rao <vrao@gogoair.com>
2017-08-12 12:19:40 -0500

    feature: Retrieve tokens from consts.py, include consts.py in gitignore

--- oauth.py
+++ oauth.py
@@ -1,13 +1,14 @@
 from flask import session
+from consts import CLIENT_SECRET, APP_ID
 import requests
 
 class OAuth(object):
     def __init__(self, url_root):
         self.host = "https://www.facebook.com/v2.8"
-        self.app_id = 1869692979931723
+        self.app_id = APP_ID
         self.redirect_uri = "{}authorized".format(url_root)
         self.facebook_url = "{}/dialog/oauth?client_id={}&redirect_uri={}".format(self.host, self.app_id, self.redirect_uri)
-        self.client_secret = "353656cc7b5bfb1891b1921487a1bdc8"
+        self.client_secret = CLIENT_SECRET
         self.host_access_token = "https://graph.facebook.com/v2.8/oauth/access_token?client_id={}&redirect_uri={}&client_secret={}".format(self.app_id, self.redirect_uri, self.client_secret)        
 
     def retrieve_access_token(self, code):

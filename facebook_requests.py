5d1d18837d70eefc84675d0ef603056c72d5f3a8
Varun <vrao@gogoair.com>
2017-08-12 23:12:01 -0500

    feature: Retrieve feed, and iterate feed to post comments

--- facebook_requests.py
+++ facebook_requests.py
@@ -7,16 +7,19 @@ class FacebookRequests(object):
         self.host_url = "https://graph.facebook.com/v2.8"
 
     def retrieve_feed(self):
+        feed_id = "{}/me".format(self.host_url)
+        feed_id_response = requests.get(feed_id, params={"access_token": self.access_token}).json()
+        print(feed_id_response)
         feed_url = "{}/me/feed".format(self.host_url)
         feed_response = requests.get(feed_url, params={"access_token": self.access_token})
         feed_json = feed_response.json()
-        return feed['data']
+        return feed_json['data']
     
     def post_comments(feed, comment):
         for post in feed:
             post_id = post['id']
             comments_url= "{}/{}/comments".format(self.host_url, post_id)
-            requests.post(comments_url, params={"access_token": self.access_token}, params={"message":comment})
+            requests.post(comments_url, params={"access_token": self.access_token, "message": comment})
         
 
 
